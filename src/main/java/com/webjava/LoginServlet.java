e8db7cdea206f817f8317d9c61317876adebff4c
m-feliciano <marcelo.feliciano2@fatec.sp.gov.br>
2021-10-02 20:45:56 -0300

    Initial commit

---
+++ src/main/java/com/webjava/LoginServlet.java
@@ -0,0 +1,22 @@
+package com.webjava;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+
+	}
+}
